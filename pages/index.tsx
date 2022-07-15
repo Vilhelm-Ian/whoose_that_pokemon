7854e75a80c0dd70aa16889230f6e7c7a755fa2b
Jovan <mariothrowsfieball@gmail.com>
2022-07-15 16:22:46 +0200

    added ability to hide pokemon

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,11 @@
 import type { NextPage } from "next";
 import { useEffect, useState } from "react";
-import Image from "next/image";
 
 const Pokemon: NextPage = () => {
   const [image, setImage] = useState(
     "https://media.istockphoto.com/vectors/loading-icon-vector-illustration-vector-id1335247217?k=20&m=1335247217&s=612x612&w=0&h=CQFY4NO0j2qc6kf4rTc0wTKYWL-9w5ldu-wF8D4oUBk="
   );
+  const [isHidden, setIsHidden] = useState(true);
 
   useEffect(() => {
     async function get_pokemon() {
@@ -21,7 +21,13 @@ const Pokemon: NextPage = () => {
   }, []);
   return (
     <div>
-      <img src={image}></img>
+      <img
+        className={`${isHidden ? "hidden_pokemon" : "visible_pokemon"}`}
+        src={image}
+        width={500}
+        height={500}
+        alt="image of a random pokemon"
+      ></img>
     </div>
   );
 };
